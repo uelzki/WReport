54d066c0519bdc22b6580a161d2844b7353cabb5
uelzki <uelzki@gmail.com>
2016-08-09 22:25:16 +0800

    WinReport_Repository
    Test number 2

---
+++ WReport/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.ViewReportBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(49, 85);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ProcessingMode = Microsoft.Reporting.WinForms.ProcessingMode.Remote;
+            this.reportViewer1.Size = new System.Drawing.Size(642, 397);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // ViewReportBtn
+            // 
+            this.ViewReportBtn.Location = new System.Drawing.Point(49, 42);
+            this.ViewReportBtn.Name = "ViewReportBtn";
+            this.ViewReportBtn.Size = new System.Drawing.Size(197, 23);
+            this.ViewReportBtn.TabIndex = 1;
+            this.ViewReportBtn.Text = "VIEW REPORT";
+            this.ViewReportBtn.UseVisualStyleBackColor = true;
+            this.ViewReportBtn.Click += new System.EventHandler(this.ViewReportBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 585);
+            this.Controls.Add(this.ViewReportBtn);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Button ViewReportBtn;
+    }
+}
+
